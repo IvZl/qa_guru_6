6b9b8e5dbf84da215bca263d44108ffe54ef2183
IvZl <shepelev007@gmail.com>
2022-01-13 22:40:20 +0300

    End writing GitHub Search Test With Listener

---
+++ src/test/java/GithubSearchTestWithListener.java
@@ -0,0 +1,29 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class GithubSearchTestWithListener {
+    @BeforeAll
+    static void beforeAll () {
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    public void searchIssueTest () {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        open("https://github.com/");
+
+        $(".header-search-input").setValue("IvZl/qa_guru_6").pressEnter();
+        $(By.linkText("IvZl/qa_guru_6")).click();
+        $(".js-sidenav-container-pjax").shouldHave(Condition.text("Issues"));
+    }
+
+}
